2135574e6e6ef8eebc7656db2ecbd4d52110e27e
sam <sampurna.chapagainnn@gmail.com>
2021-10-01 18:12:10 +0545

    clean up files and reuse custom hook to display blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const {id} = useParams();
+  const {data:blog, pending, error} = useFetch('http://localhost:8000/blogs/' + id);
+  console.log(blog);
   return (
     <div className="blog-details">
       <h2>Blog Details {id} </h2>
+      {blog && (
+        <div>
+          <h1>{blog.title}</h1>
+          <p>{blog.description}</p>
+          <p>Written by: {blog.author}</p>
+          </div>
+      )}
     </div>
   );
 }
