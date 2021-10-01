b6b9f528a80bb3008ec8021e0aa87b6f8d200f8f
sam <sampurna.chapagainnn@gmail.com>
2021-10-01 16:23:15 +0545

    link each blogs to their show page-bloglists

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, handleDelete}) => {
   return (
     <div className="blog-list">
       {blogs.map((blog)=>(
         <div className="blog" key={blog.title}>
-          <h1>{blog.title}</h1>
-          <h3>{blog.description}</h3>
-          <button onClick={()=> handleDelete(blog.id)}>Delete</button>
+          <Link to={`/blogs/${blog.id}`}>
+            <h1>{blog.title}</h1>
+            <h3>{blog.description}</h3>
+          </Link>
         </div>
       ))}
     </div>
