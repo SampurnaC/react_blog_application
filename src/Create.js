bcc6580ff548278838cbbce9a78633d9ae498408
sam <sampurna.chapagainnn@gmail.com>
2021-10-01 23:08:17 +0545

    add feature to create a new blog and redirect to root path after blog created

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,34 @@
+import { useHistory } from "react-router";
+import { useState } from "react/cjs/react.development";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const history = useHistory();
+  const createBlog=(e)=>{
+    e.preventDefault();
+    const blog={title, description};
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(()=>{
+      console.log("new blog added");
+      history.push('/');
+    })
+  }
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={createBlog}>
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} />
+        <label>Body:</label>
+        <input type="text" value={description} onChange={(e)=>setDescription(e.target.value)} />
+        <button>Create Blog</button>
+      </form>
+      {title}
+      {description}
     </div>
   );
 }
