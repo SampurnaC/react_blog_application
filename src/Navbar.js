06c0b4523e293eada4abea736993e6d796b150ab
sam <sampurna.chapagainnn@gmail.com>
2021-11-01 18:33:56 +0545

    add nav bar links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div>
       I am from Navbar
+      <Link to="/">Home</Link>
+      <br />
+      <Link to='create'>Create</Link>
     </div>
   );
 }
