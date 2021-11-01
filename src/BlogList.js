18870e3f3cf02d2e1fd445405fd9c371b27bcf6e
sam <sampurna.chapagainnn@gmail.com>
2021-11-01 18:34:11 +0545

    add search feature to blog lists

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,22 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import { useState } from 'react/cjs/react.development';
+
 
 const BlogList = ({blogs, handleDelete}) => {
+  const[searchItem, setSearchItem] = useState('');
   return (
+    
     <div className="blog-list">
-      {blogs.map((blog)=>(
+      <input type="text" placeholder="Search..." value={searchItem} onChange={(e)=>{setSearchItem(e.target.value)}}/>
+      {blogs.filter((val)=>{
+        if (searchItem==""){
+          return val;
+        }
+        else if (val.title.toLowerCase().includes(searchItem.toLowerCase())) {
+          return val;
+        }
+      }).map((blog)=>(
         <div className="blog" key={blog.title}>
           <Link to={`/blogs/${blog.id}`}>
             <h1>{blog.title}</h1>
